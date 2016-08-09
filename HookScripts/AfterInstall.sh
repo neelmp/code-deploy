468419d511c23e717dd5cd27239c8ca9ccce343e
neelmp <neel.online@gmail.com>
2016-08-09 15:02:52 +1000

    Update AfterInstall.sh

--- HookScripts/AfterInstall.sh
+++ HookScripts/AfterInstall.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 chown apache:www /var/www/html/resource
 chmod 755 /var/www/html/resource
+find /var/www/html/resource -type d -exec chown apache:www {} \;
+find /var/www/html/resource -type f -exec chown apache:www {} \;
 find /var/www/html/resource -type d -exec chmod 755 {} \;
 find /var/www/html/resource -type f -exec chmod 644 {} \;
