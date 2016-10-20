135d487d4495aaa79df76eb52ad0852572832992
neelmp <neel.online@gmail.com>
2016-10-20 12:25:22 +1100

    Delete AfterInstall.sh

--- HookScripts/AfterInstall.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-chown -R apache:www /var/www/html/resource
-chmod 755 /var/www/html/resource
-find /var/www/html/resource -type d -exec chmod 755 {} \;
-find /var/www/html/resource -type f -exec chmod 644 {} \;
